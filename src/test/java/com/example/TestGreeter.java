78b9a124efc131bba64127496f778b07c22d82f5
root <root@ip-172-31-36-83.ap-south-1.compute.internal>
2022-09-09 07:21:08 +0000

    changed TC1

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -23,8 +23,9 @@ public class TestGreeter {
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
     String someone = "World";
+    String anotherOne = "Someone";
 
-    assertThat(greeter.greet(someone), containsString(someone));
+    assertThat(greeter.greet(anotherOne), containsString(anotherOne));
   }
 
   @Test
